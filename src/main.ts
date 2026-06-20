36a84dfc8d6e21231edd031b7487c21bfe8f1d2a
iuhiu lklk <samuelgarrotearroyo@gmail.com>
2026-06-20 14:42:29 +0200

    Update CORS for production frontend

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:4200' });
+  app.enableCors({
+    origin: [
+      'http://localhost:4200',
+      'https://angular-enterprise-dashboard-psi.vercel.app',
+    ],
+  });
   await app.listen(3000);
 }
 bootstrap();
